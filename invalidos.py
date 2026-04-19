42ca5a818909d178c8fad6091ed03b37fac66a06
GabSoares404 <gabmix05@gmail.com>
2026-04-19 20:26:33 -0300

    Correção invalidos.py

--- invalidos.py
+++ invalidos.py
@@ -15,15 +15,19 @@ for rowid, url in registros:
     if url.startswith("http://"):
         nova_url = url.replace("http://", "https://", 1)
 
-        cursor.execute("""
-            UPDATE arquivos 
-            SET url = ?, status = 0, erro_log = NULL
-            WHERE rowid = ?
-        """, (nova_url, rowid))
-        a = a+1
-        print(f"🔄 {url} -> {nova_url} | status resetado")
+        try:
+            cursor.execute("""
+                UPDATE arquivos 
+                SET url = ?, status = 0, erro_log = NULL
+                WHERE rowid = ?
+            """, (nova_url, rowid))
+            conn.commit()  # Salva a alteração imediatamente
+            a = a+1
+            print(f"🔄 {url} -> {nova_url} | status resetado")
+        except sqlite3.IntegrityError:
+            conn.rollback()  # Desfaz a operação com erro
+            print(f"⚠️ Já existe uma versão https para: {url} | Ignorando duplicata")
 
-conn.commit()
 conn.close()
 
 print(f"✅ {a} URLs corrigidas e prontas para retry!")
